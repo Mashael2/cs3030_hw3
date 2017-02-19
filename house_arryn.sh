715fc5960c82537e0b883eaeed0e211fd8724cc1
Mashael Alabbad <malabbad1@mail.weber.edu>
2017-02-19 12:22:42 -0700

    test

--- house_arryn.sh
+++ house_arryn.sh
@@ -13,17 +13,42 @@
 #ser -o nounset                    # Treat unset variables as an error
 
 outFile="20CenturyPresidents.txt"
+
 help() 
 {
-	echo "Usage ./ house_arryn.sh -s sedsrc -a awksrc -i inputFile"
+	echo "Usage ./house_arryn.sh -s sedsrc -a awksrc -i inputFile"
 	exit 1
 }
 
-if [ "$1" == "--help" ] 
+reqHelp()
+{
+	echo "Missing arguments"
+	echo "Usage ./house_arryn.sh -s sedsrc -a awksrc -i inputFile"
+	exit 1
+}
+
+message()
+{
+	echo "Your output file will be save in the 20CenturyPresidents.txt"
+	exit 1
+}
+
+if [[ "$1" == "--help" ]] 
 then
 # Call help function
 	help
 fi
+
+# Verify that sedsrc and awksrc and iFile are defined
+if [[ "$1" != "-s" ]] || [[ "$3" != "-a" ]] || [[ "$5" != "-i" ]]
+then
+	reqHelp
+fi
+
+#if [[ "$1" == "-s" ]] || [[ "$3" == "-a" ]] || [[ "$5" != "-i" ]]
+#then
+#	reqHelp
+#fi
 
 while getopts ":s:a:i:" opt
 do
@@ -39,10 +64,7 @@ do
 	esac
 done
 
-# Verify that sedsrc and swksrc and iFile are defined
-
 # Call Sed script to modify data
 sed -f $sedsrc $iFile | awk -f $awksrc > $outFile
-
 
 exit 0
